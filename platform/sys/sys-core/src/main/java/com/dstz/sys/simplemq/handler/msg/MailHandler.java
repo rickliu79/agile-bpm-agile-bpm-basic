4a59313e5c77371923ed18c6de61065b3b11ad9e
aschs <632266504@qq.com>
2018-09-13 00:57:00 +0800

    去掉emailUtil

--- platform/sys/sys-core/src/main/java/com/dstz/sys/simplemq/handler/msg/MailHandler.java
+++ platform/sys/sys-core/src/main/java/com/dstz/sys/simplemq/handler/msg/MailHandler.java
@@ -3,17 +3,16 @@ package com.dstz.sys.simplemq.handler.msg;
 import java.util.List;
 
 import javax.annotation.Resource;
-import javax.mail.MessagingException;
 
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSON;
-import com.dstz.base.core.util.PropertyUtil;
 import com.dstz.base.core.util.StringUtil;
 import com.dstz.org.api.model.IUser;
 import com.dstz.sys.api.jms.model.msg.NotifyMessage;
 import com.dstz.sys.api.service.SysIdentityConvert;
-import com.dstz.sys.util.EmailUtil;
+
+import cn.hutool.extra.mail.MailUtil;
 
 /**
  * 邮件消息处理器。
@@ -42,16 +41,14 @@ public class MailHandler extends AbsNotifyMessageHandler<NotifyMessage> {
 
 	@Override
 	public boolean sendMessage(NotifyMessage notifMessage) {
-		String fromEmail = PropertyUtil.getProperty("mail.address");
-	  
         List<IUser> recievers = sysIdentityConvert.convert2Users(notifMessage.getReceivers());
         
         for (IUser reciver : recievers) {
             String email = reciver.getEmail();
             if (StringUtil.isEmpty(email)) continue;
             try {
-                EmailUtil.sendEmail(email, "", "", fromEmail, notifMessage.getSubject(), notifMessage.getHtmlContent());
-            } catch (MessagingException e) {
+            	MailUtil.send(email, notifMessage.getSubject(), notifMessage.getHtmlContent(), true);
+            } catch (Exception e) {
             	LOG.error(JSON.toJSONString(notifMessage));
             	LOG.error("发送邮件失败！",e);
             }
