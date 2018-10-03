770997a784b50bc8a288b273b6e9ac2f96ab8df8
jeff <for_office@qq.com>
2018-10-03 17:57:19 +0800

    添加 org_group_reldef 表更新

--- _doc/mysql/changesql.sql
+++ _doc/mysql/changesql.sql
@@ -2,72 +2,79 @@
 
 
 ALTER TABLE `org_group` 
-ADD COLUMN `create_time_` datetime(0) NULL COMMENT '创建时间',
+ADD COLUMN `create_time_` datetime NULL COMMENT '创建时间',
 ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
-ADD COLUMN `update_time_` datetime(0) NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
 ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
 	
 ALTER TABLE `org_group_user` 
-ADD COLUMN `create_time_` datetime(0) NULL COMMENT '创建时间',
+ADD COLUMN `create_time_` datetime NULL COMMENT '创建时间',
 ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
-ADD COLUMN `update_time_` datetime(0) NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
 ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
 
 ALTER TABLE `org_role` 
-ADD COLUMN `create_time_` datetime(0) NULL COMMENT '创建时间',
+ADD COLUMN `create_time_` datetime NULL COMMENT '创建时间',
 ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
-ADD COLUMN `update_time_` datetime(0) NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
 ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
 
 ALTER TABLE `org_group_rel` 
-ADD COLUMN `create_time_` datetime(0) NULL COMMENT '创建时间',
+ADD COLUMN `create_time_` datetime NULL COMMENT '创建时间',
 ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
-ADD COLUMN `update_time_` datetime(0) NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
 ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
+
+ALTER TABLE `org_group_reldef` 
+ADD COLUMN `create_time_` datetime NULL COMMENT '创建时间',
+ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
+
+
 
 ALTER TABLE `org_user` 
 ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
-ADD COLUMN `update_time_` datetime(0) NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
 ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
 
 ALTER TABLE `org_user_role` 
-ADD COLUMN `create_time_` datetime(0) NULL COMMENT '创建时间',
+ADD COLUMN `create_time_` datetime NULL COMMENT '创建时间',
 ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
-ADD COLUMN `update_time_` datetime(0) NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
 ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
 
 	
 
 ALTER TABLE `bus_table` 
-ADD COLUMN `create_time_` datetime(0) NULL COMMENT '创建时间',
+ADD COLUMN `create_time_` datetime NULL COMMENT '创建时间',
 ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
-ADD COLUMN `update_time_` datetime(0) NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
 ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
 
 ALTER TABLE `bus_permission` 
-ADD COLUMN `create_time_` datetime(0) NULL COMMENT '创建时间',
+ADD COLUMN `create_time_` datetime NULL COMMENT '创建时间',
 ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
-ADD COLUMN `update_time_` datetime(0) NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
 ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
 
 ALTER TABLE `bus_object` 
-ADD COLUMN `create_time_` datetime(0) NULL COMMENT '创建时间',
+ADD COLUMN `create_time_` datetime NULL COMMENT '创建时间',
 ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
-ADD COLUMN `update_time_` datetime(0) NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
 ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
 
 ALTER TABLE `bus_column` 
-ADD COLUMN `create_time_` datetime(0) NULL COMMENT '创建时间',
+ADD COLUMN `create_time_` datetime NULL COMMENT '创建时间',
 ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
-ADD COLUMN `update_time_` datetime(0) NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
 ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
 
 ALTER TABLE `bus_column_ctrl` 
-ADD COLUMN `create_time_` datetime(0) NULL COMMENT '创建时间',
+ADD COLUMN `create_time_` datetime NULL COMMENT '创建时间',
 ADD COLUMN `create_by_` varchar(64) NULL COMMENT '创建人' AFTER `create_time_`,
-ADD COLUMN `update_time_` datetime(0) NULL COMMENT '更新时间' AFTER `create_by_`,
+ADD COLUMN `update_time_` datetime NULL COMMENT '更新时间' AFTER `create_by_`,
 ADD COLUMN `update_by_` varchar(64) NULL  COMMENT '更新人' AFTER `update_time_`;
-
 
 
 
