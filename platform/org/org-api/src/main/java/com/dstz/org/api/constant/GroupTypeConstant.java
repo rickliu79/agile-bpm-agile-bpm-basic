f14b0879587ffc2b998e8ed959ce9655eba30fbd
jeff <for_office@qq.com>
2018-11-23 20:14:44 +0800

    组织key 修改下

--- platform/org/org-api/src/main/java/com/dstz/org/api/constant/GroupTypeConstant.java
+++ platform/org/org-api/src/main/java/com/dstz/org/api/constant/GroupTypeConstant.java
@@ -12,7 +12,7 @@ public enum GroupTypeConstant {
     ORG("org", "组织"),
     ROLE("role", "角色"),
     JOB("job", "职位"),
-    POSITION("position", "岗位");
+    POSITION("post", "岗位");
     private String key;
     private String label;
 
