a17bdc07c3fe4c0093d0218fb90109c245ec56df
jeff <for_office@qq.com>
2018-12-16 23:28:10 +0800

    代码调整

--- base/base-db/src/main/java/com/dstz/base/db/model/query/DefaultQueryField.java
+++ base/base-db/src/main/java/com/dstz/base/db/model/query/DefaultQueryField.java
@@ -1,14 +1,12 @@
 package com.dstz.base.db.model.query;
 
-import java.util.Date;
+import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
 
-import com.dstz.base.api.constant.StringConstants;
 import com.dstz.base.api.query.QueryField;
 import com.dstz.base.api.query.QueryOP;
 
-import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.collection.CollectionUtil;
 
 /**
  * 默认条件接口实现类。
@@ -49,38 +47,32 @@ public class DefaultQueryField implements QueryField {
     /**
      * 针对in查询方式，根据传入的value的类型做不同的处理。 value 是 string，则分隔字符串，然后合并为符合in规范的字符串。
      * value 是 list，则直接合并为符合in规范的字符串。
-     *
+     * TODO 待改造
      * @return
      */
     private String getInValueSql() {
-        if (value instanceof String) { // 字符串形式，通过逗号分隔
-            StringBuilder sb = new StringBuilder();
-            sb.append("(");
-            StringTokenizer st = new StringTokenizer(value.toString(), ",");
-            while (st.hasMoreTokens()) {
-                sb.append("\'");//oracle参数只支持'
-                sb.append(st.nextToken());
-                sb.append("\'");
-                sb.append(",");
-            }
-            sb = new StringBuilder(sb.substring(0, sb.length() - 1));
-            sb.append(")");
-            return sb.toString();
-        } else if (value instanceof List) { // 列表形式
-            List<Object> objList = (List<Object>) value;
-            StringBuilder sb = new StringBuilder();
-            sb.append("(");
-            for (Object obj : objList) {
-                sb.append("\"");
-                sb.append(obj.toString());
-                sb.append("\"");
-                sb.append(",");
-            }
-            sb = new StringBuilder(sb.substring(0, sb.length() - 1));
-            sb.append(")");
-            return sb.toString();
+    	List listValue =  null;
+    	if(value instanceof String) {
+    		listValue = Arrays.asList(((String)value).split(","));
+    	}
+    	if (value instanceof List) {
+    		listValue = (List) value ;
+    	}
+    	
+    	if(CollectionUtil.isEmpty(listValue)) return "";
+    	
+        StringBuilder inSqlStr = new StringBuilder("(");
+        for (Object obj : listValue) {
+        	if(obj == null || obj.equals("")) continue;
+        	
+            inSqlStr.append("\"");
+            inSqlStr.append(obj.toString());
+            inSqlStr.append("\"");
+            inSqlStr.append(",");
         }
-        return "";
+        inSqlStr = new StringBuilder(inSqlStr.substring(0, inSqlStr.length() - 1));
+        inSqlStr.append(")");
+        return inSqlStr.toString();
     }
 
     public String getField() {
@@ -177,26 +169,4 @@ public class DefaultQueryField implements QueryField {
         return sql;
     }
     
-
-    private String getBetweenSql() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(" between ");
-        if (this.value instanceof List) {
-            List<Object> objList = (List<Object>) value;
-            for (int i = 0; i < objList.size(); i++) {
-                Object obj = objList.get(i);
-                if (i == 1) {
-                    sb.append(" and ");
-                }
-                if (obj instanceof Date) {
-                    String dateString = DateUtil.format((Date) obj, StringConstants.DATE_FORMAT_DATETIME);
-                    sb.append("\"" + dateString + "\"");
-                } else {
-                    sb.append("\"" + obj.toString() + "\"");
-                }
-            }
-        }
-        sb.append(" ");
-        return sb.toString();
-    }
 }
