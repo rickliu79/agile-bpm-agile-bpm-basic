fc06e966b3ece6eeb19c50c90db8861f5ce9ce13
jeff <for_office@qq.com>
2019-01-12 00:51:42 +0800

    建表统一大小写

--- _doc/mysql/dataInit/7.wf.init.sql
+++ _doc/mysql/dataInit/7.wf.init.sql
@@ -1,7 +1,7 @@
 -- activiti 初始化数据
-INSERT INTO `act_ge_property` (`NAME_`, `VALUE_`, `REV_`) VALUES ('next.dbid', '0', '0');
-INSERT INTO `act_ge_property` (`NAME_`, `VALUE_`, `REV_`) VALUES ('schema.history', 'create(5.22.0.0) upgrade(5.21.0.0->5.22.0.0)', '2');
-INSERT INTO `act_ge_property` (`NAME_`, `VALUE_`, `REV_`) VALUES ('schema.version', '5.22.0.0', '2');
+INSERT INTO `ACT_GE_PROPERTY` (`NAME_`, `VALUE_`, `REV_`) VALUES ('next.dbid', '0', '0');
+INSERT INTO `ACT_GE_PROPERTY` (`NAME_`, `VALUE_`, `REV_`) VALUES ('schema.history', 'create(5.22.0.0) upgrade(5.21.0.0->5.22.0.0)', '2');
+INSERT INTO `ACT_GE_PROPERTY` (`NAME_`, `VALUE_`, `REV_`) VALUES ('schema.version', '5.22.0.0', '2');
 
 
 -- 流程管理
