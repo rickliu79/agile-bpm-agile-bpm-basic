482b4b1ce2c3f273dd8281f5a3a72f5169f1e4fe
jeff <for_office@qq.com>
2019-02-02 23:28:58 +0800

    百度编辑器 修改偶尔无效的 bug修复

--- bpm-explorer/assets/js/common/base/baseDirective.js
+++ bpm-explorer/assets/js/common/base/baseDirective.js
@@ -312,11 +312,9 @@ var directive = angular.module("baseDirective", [ "base" ])
 			editor.render(element[0]);
 			// 百度UEditor数据更新时，更新Model
 			editor.addListener('contentChange', function() {
-				setTimeout(function() {
-					scope.$apply(function() {
-						ngModel.$setViewValue(editor.getContent());
-					});
-				}, 0);
+				scope.$apply(function() {
+					ngModel.$setViewValue(editor.getContent());
+				});
 			});
 			
 			/**
@@ -339,11 +337,7 @@ var directive = angular.module("baseDirective", [ "base" ])
 			};
 			
 			scope.$on("beforeSaveEvent", function(event, data) {
-				//源码模式
-				if(editor.queryCommandState('source')=="1"){
-					data.pass = false;
-					$.Toast.error("编辑器在源码模式下无法保存");
-				}
+				ngModel.$setViewValue(editor.getContent());
 			});
 		}
 	}
